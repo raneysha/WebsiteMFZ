d9a0f0eeeee14014196e4fa03875eb5e99b220ef
Cahyo Dwi Setiono <cahyo123dwi@gmail.com>
2023-01-28 20:28:15 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        Mallanna: ['"Mallanna"'],
+        Steelfish: ['"Steelfish"']
+      }
+    },
+  },
+  plugins: [],
+}
